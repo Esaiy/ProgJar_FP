2964803ebb7fc14be17c3f9f2ca9fb5059448737
ilhambayhaqi <ilham.bhq28@gmail.com>
2021-07-03 08:40:23 +0700

    Create Chat Server, add game in command

---
+++ client/client.py
@@ -0,0 +1,274 @@
+import socket
+import sys
+import threading
+import os
+import pickle
+import ntpath
+from getpass import getpass
+from tkinter import Tk # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+buff_size = 65535
+HOST = '127.0.0.1'
+PORT = 5000
+
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_client.connect((HOST, PORT))
+    
+class Account:
+    def __init__ (self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.friend = set()
+
+def clear(args):
+    if os.name == 'nt':
+        _ = system('cls')
+    else:
+        _ = os.system('clear')
+
+def header_page():
+    clear('')
+    print('Welcome to Esaiyy Play')
+    print('==============================')
+
+def get_request_header(requestType, lenRequest):
+    header = b''
+    header += requestType.encode('utf-8') + b'\n'
+    header += str(lenRequest).encode('utf-8') + b'\n'
+    return header
+
+def parseRequest(request):
+    splitRequest = (request.split(b'\n', 2))
+    requestType = splitRequest[0].decode('utf-8')
+    lenData = int(splitRequest[1].decode('utf-8'))
+    data = splitRequest[2]
+    return requestType, lenData, data
+
+def helper(args):
+    print('''Manual for Esaiyy Play\nCommand:
+    help - show all command available
+    add - add user to friend list
+        add [user] - add user to your friend
+    friend - show my friend list
+    chat - chat to specific user or broadcast
+        chat [user] - chat to account user
+        chat [bcast] - broadcast chat
+    send - send a file
+        send [user] - send a file to use
+    game - play a battlefield game
+        game [create] - create a game room
+        game [join] [roomNo] - join to game specific room number
+        
+    ''')
+    return
+
+def friendlist(args):
+    dataRequest = pickle.dumps(tuple())
+    headerRequest = get_request_header('friendlist', len(dataRequest))
+    socket_client.send(headerRequest + dataRequest)
+    return
+
+def chat(args):
+    data = dict()
+
+    data[0] = args[1] if len(args) > 1 else input('<App>: Send to (use [user_id] or bcast) :\n')
+    data[1] = input('<App>: Type your message :\n')
+
+    dataRequest = pickle.dumps((data,))
+    headerRequest = get_request_header('chat', len(dataRequest))
+    socket_client.send(headerRequest + dataRequest)    
+    return
+
+def addfriend(args):
+    user_id = args[1] if len(args) > 1 else input('<App>: Add Friend ID :\n')
+    dataRequest = pickle.dumps((user_id, ))
+    headerRequest = get_request_header('addfriend', len(dataRequest))
+    socket_client.send(headerRequest + dataRequest)  
+    return
+
+def sendfile(args):
+    user_id = args[1] if len(args) > 1 else input('<App>: Send to : (user_id)\n')
+
+    Tk().withdraw()
+    filepath = askopenfilename()
+    if filepath:
+        f = open(filepath, 'rb')
+        filename = ntpath.basename(filepath)
+        data = f.read()
+
+        dataRequest = pickle.dumps((user_id, filename, data))
+        # print(dataRequest)
+        headerRequest = get_request_header('sendfile', len(dataRequest))
+        socket_client.sendall(headerRequest + dataRequest)
+    
+    return
+
+def playgame(args):
+    roomCommand = args[1] if len(args) > 1 else input('<App>: Create or Join room? (create|join)\n')
+    if(roomCommand == 'create'):
+        print("Create")
+    elif (roomCommand == 'join'):
+        print('Join the game')
+    return
+
+def commandError(args):
+    print('<App>: Command Not Found')
+
+def commandSwitch(args):
+    commandAvailable = {
+        'help' : helper,
+        'friend' : friendlist,
+        'add' : addfriend,
+        'chat' : chat,
+        'send' : sendfile,
+        'clear' : clear,
+        'game' : playgame,
+    }
+    args = args.split(' ')
+    # args = [''] if len(args) == 0 else args
+    commandAvailable.get(args[0], commandError)(args)
+
+def read_message(myaccount):
+    while True:
+        response = socket_client.recv(buff_size)
+        responseType, lenData, data = parseRequest(response)
+
+        dataRemain = lenData - len(data)
+        while dataRemain > 0:
+            response = socket_client.recv(buff_size)
+            data += response
+            dataRemain -= len(response)
+
+        data = pickle.loads(data)
+
+        if responseType == 'addfriend':
+            if data[1] == 'success':
+                print('<App>: {} now added to your friend list'.format(data[2].id))
+            else:
+                print('<App>: Cannot add user!')
+            print()
+
+        elif responseType == 'friendlist':
+            print('<App>:\n== Your Friend ==')
+            if data[1]:
+                for idx, user in enumerate(data[1]):
+                    print('  {}. {}'.format(idx + 1, user))
+            else:
+                print('No one in your friend list') 
+            print()             
+        
+        elif responseType == 'chat':
+            if data[1] == 'failed':
+                print("<App>: {}".format(data[2]))
+            else:
+                senderid, sendername, message = data[2]
+                print("<{}> {}: {}".format(senderid, sendername, message))
+            print()
+
+        elif responseType == 'sendfile':
+            if data[1] == 'failed':
+                print("<App>: {}".format(data[2]))
+            else:
+                senderid, sendername, filename, filedata = data[2]
+                print("<App>: <{}> {} send you a file ".format(senderid, sendername))
+                
+                dir = os.listdir(os.getcwd())
+                if not(myaccount.id in dir):
+                    os.mkdir(myaccount.id)
+
+                f = open(myaccount.id + '/' + filename, 'wb')
+                f.write(filedata)
+                f.close()
+                
+            print()
+
+def dasboard(status, myAccount):
+    header_page()
+    print(status, end='')
+    print('Hello, ' + myAccount.name + '\n')
+    print('Type "help" to see all available command \n')
+    
+    thread = threading.Thread(target=read_message, args=(myAccount,))
+    thread.daemon = True
+    thread.start()
+
+    try:
+        while True:
+            command = input()
+            commandSwitch(command)
+    except KeyboardInterrupt:
+        socket_client.close()
+        sys.exit()
+
+def register(status):
+    try:
+        header_page()
+        print('Register an account\n')
+        print(status, end='')
+
+        id = input('Enter username: ')
+        name = input('Enter your name: ')
+        password = getpass('Enter password: ')
+        newAccount = Account(id, name, password)
+
+
+        dataRequest = pickle.dumps((newAccount,))
+        headerRequest = get_request_header('register', len(dataRequest))
+
+        socket_client.send(headerRequest + dataRequest)
+        response = socket_client.recv(buff_size)
+        response = pickle.loads(response)
+
+        if response[0] == 'failed':
+            register('Account is already Exist\n')
+        else:
+            dasboard('Account Created, you\'re login now\n', response[1])
+        return
+    except KeyboardInterrupt:
+        return welcome_page()
+
+def login(status):
+    try:
+        header_page()
+        print('Login to chat\n')
+        print(status, end='')
+
+        id = input('Enter username: ')
+        password = getpass('Enter password: ')
+
+        dataRequest = pickle.dumps((id, password))
+        headerRequest = get_request_header('login', len(dataRequest))
+
+        socket_client.send(headerRequest + dataRequest)
+        response = socket_client.recv(buff_size)
+        response = pickle.loads(response)
+
+        if response[0] == 'failed':
+            login('Login Failed, please check your credential\n')
+        else:
+            dasboard('Login success\n', response[1])
+        return
+    except KeyboardInterrupt:
+        return welcome_page()
+        
+def welcome_page():
+    try:
+        header_page()
+        status = ''
+        islogin = input('Have an account? (y/n) : ')
+        if islogin == 'n':
+            register(status)
+        elif islogin == 'y':
+            login(status)
+        else:
+            print('Byee!!')
+    except KeyboardInterrupt:
+        sys.exit()
+
+def main():  
+    welcome_page()
+
+if __name__ == '__main__':
+    main()
---
+++ server/server.py
@@ -0,0 +1,233 @@
+import socket
+import pickle
+import threading
+
+buff_size = 65535
+HOST = '0.0.0.0'
+PORT = 5000
+clients = {}
+
+class AccountManager():
+    def __init__ (self):
+        self.account = {}
+        self.online = {}
+    def check_account(self, id):
+        return self.account.get(id, None)
+
+    def add_account(self, account):
+        check = self.check_account(account.id)
+        if check == None:
+            self.account[account.id] = account
+            return self.account[account.id]
+        else:
+            return None
+
+    def check_online(self, account):
+        if type(account) == Account:
+            return self.online.get(account.id, None)
+        elif type(account) == str:
+            return self.online.get(account, None)
+        
+    def add_online(self, account, socket_client, address_client):
+        if not(self.check_online(account)):
+            self.online[account.id] = (socket_client, address_client)
+            return account
+        return None
+        
+    def set_disconnected(self, account):
+        print("Account " + account.id + " disconnected")
+        del self.online[account.id]
+
+class Account:
+    def __init__ (self, id, name, password):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.friend = set()
+    def get_friendlist(self):
+        return self.friend
+    def check_friend(self, account):
+        return account in self.friend
+
+def get_response_header(requestType, lenRequest):
+    header = b''
+    header += requestType.encode('utf-8') + b'\n'
+    header += str(lenRequest).encode('utf-8') + b'\n'
+    return header
+
+def parseRequest(request):
+    splitRequest = (request.split(b'\n', 2))
+    requestType = splitRequest[0].decode('utf-8')
+    lenData = int(splitRequest[1].decode('utf-8'))
+    data = splitRequest[2]
+    return requestType, lenData, data
+
+def commandHandler(socket_client, address_client):
+    currentAccount = None
+    while True:
+        request = socket_client.recv(buff_size)
+        if request:
+            requestType, lenData, data = parseRequest(request)
+            print(lenData)
+            dataRemain = lenData - len(data)
+            
+            while dataRemain > 0:
+                request = socket_client.recv(buff_size)
+                data += request
+                dataRemain -= len(request)
+
+            data = pickle.loads(data)
+
+            if requestType == 'register' :
+                currentAccount = accountManager.add_account(data[0])
+                response = dict()
+                if currentAccount:
+                    response[0] = 'success'
+                    response[1] = currentAccount
+                    print("user {} has been created".format(currentAccount.id))
+                    accountManager.add_online(currentAccount, socket_client, address_client)
+                else:
+                    response[0] = 'failed'
+                socket_client.send(pickle.dumps(response))
+                
+            elif requestType == 'login' :
+                currentAccount = accountManager.check_account(data[0])
+                response = dict()
+                if currentAccount and not(accountManager.check_online(currentAccount)) and data[1] == currentAccount.password:
+                    response[0] = 'success'
+                    response[1] = currentAccount
+                    print("{} is online".format(currentAccount.id))
+                    accountManager.add_online(currentAccount, socket_client, address_client)
+                else:
+                    response[0] = 'failed'
+                    print("Failed login for user {}".format(data[0]))
+                    currentAccount = None    
+                socket_client.send(pickle.dumps(response))
+
+            elif requestType == 'friendlist':
+                response = dict()
+                response[1] = currentAccount.get_friendlist()
+                responseData = pickle.dumps(response)
+                responseHeader = get_response_header(requestType, len(responseData))
+                print('Request friend list for user {}'.format(currentAccount.id))
+                print(responseHeader + responseData)
+                socket_client.sendall(responseHeader + responseData)
+
+            elif requestType == 'addfriend':
+                userTarget = accountManager.check_account(data[0])
+                response = dict()
+           
+                if userTarget and not(userTarget == currentAccount):
+                    currentAccount.friend.add(userTarget.id)
+                    response[1] = 'success'
+                    response[2] = userTarget
+                else:
+                    response[1] = 'failed'
+
+                responseData = pickle.dumps(response)
+                responseHeader = get_response_header(requestType, len(responseData))
+                socket_client.sendall(responseHeader + responseData)
+
+            elif requestType == 'chat' :
+                requestData = data[0]
+                destination = requestData[0]
+                message = requestData[1]
+                
+                response = dict()
+                if destination == 'bcast' : 
+                    send_broadcast(currentAccount, message, socket_client)
+                else:
+                    send_message(currentAccount, destination, message, socket_client) 
+            
+            elif requestType == 'sendfile':
+                destination = data[0]
+                filename = data[1]
+                filedata = data[2]
+                send_file(currentAccount, destination, filename, filedata, socket_client)
+
+        else:
+            if currentAccount:
+                accountManager.set_disconnected(currentAccount)
+            break
+    return
+
+def send_broadcast(currentAccount, message, socket_client):
+    for destination in currentAccount.friend:
+        send_message(currentAccount, destination, message, socket_client)
+    return
+
+def send_file(currentAccount, destination, filename, filedata, socket_client):
+    response = dict()
+    requestType = 'sendfile'
+    if not(destination == currentAccount.id):
+        if(currentAccount.check_friend(destination)):
+            checkDestination = accountManager.check_online(destination)
+            if checkDestination:
+                dest_socket, _ = checkDestination
+                response[1] = 'success'
+                response[2] = (currentAccount.id, currentAccount.name, filename, filedata)
+                responseData = pickle.dumps(response)
+                responseHeader = get_response_header(requestType, len(responseData))
+                dest_socket.sendall(responseHeader + responseData)
+                return
+            else:
+                response[2] = destination + ' is not online'
+        else:
+            response[2] = destination + ' is not your friend'
+    else:
+        response[2] = 'Cannot sent file to yourself'
+
+    response[1] = 'failed'
+    responseData = pickle.dumps(response)
+    responseHeader = get_response_header(requestType, len(responseData))
+    socket_client.sendall(responseHeader + responseData)
+    return
+
+def send_message(currentAccount, destination, message, socket_client):
+    response = dict()
+    requestType = 'chat'
+    if not(destination == currentAccount.id):
+        if(currentAccount.check_friend(destination)):
+            checkDestination = accountManager.check_online(destination)
+            if checkDestination:
+                dest_socket, _ = checkDestination
+                response[1] = 'success'
+                response[2] = (currentAccount.id, currentAccount.name, message)
+                responseData = pickle.dumps(response)
+                responseHeader = get_response_header(requestType, len(responseData))
+                dest_socket.sendall(responseHeader + responseData)
+                return
+            else:
+                response[2] = destination + ' is not online'
+        else:
+            response[2] = destination + ' is not your friend'
+    else:
+        response[2] = 'Cannot sent message to yourself'
+    
+    response[1] = 'failed'
+    responseData = pickle.dumps(response)
+    responseHeader = get_response_header(requestType, len(responseData))
+    socket_client.sendall(responseHeader + responseData)
+    return
+
+try:
+    accountManager = pickle.load(open('accountmanager.pkl', 'rb'))
+except:
+    accountManager = AccountManager()
+
+def main():
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.bind((HOST, PORT))
+    socket_server.listen(5)
+    
+    try:
+        while True:
+            socket_client, address_client = socket_server.accept()
+
+            thread_client = threading.Thread(target=commandHandler, args=(socket_client, address_client))
+            thread_client.start()
+    except KeyboardInterrupt:
+        pickle.dump(accountManager, open('accountmanager.pkl', 'wb'))
+
+if __name__ == '__main__':
+    main()
